d78efcba5950cbe16bf1d1b73f6ccefce83e2522
Ilya_kamar <ilyakamar@gmail.com>
2017-06-28 23:00:24 +0300

    Initial commit

---
+++ app/src/main/java/com/loftblog/fb_test_3/EmailPasswordActivity.java
@@ -0,0 +1,133 @@
+package com.loftblog.fb_test_3;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class EmailPasswordActivity extends AppCompatActivity implements View.OnClickListener { // Start EmailPasswordActivity
+
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    String TAG;
+
+    private EditText et_email;     /// =ETemail
+    private EditText et_password; // =ETpassword
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { // start onCreate
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    // User is signed in
+
+                    Intent intent = new Intent(EmailPasswordActivity.this, ListTasks.class);
+                    startActivity(intent);
+
+
+
+                } else {
+                    // User is signed out
+
+                }
+                // ...
+            }
+        };
+
+
+        et_email = (EditText) findViewById(R.id.ET_email);
+        et_password = (EditText) findViewById(R.id.ET_password);
+
+
+        findViewById(R.id.btn_Login).setOnClickListener(this);
+        findViewById(R.id.btn_LinkToRegisterScreen).setOnClickListener(this);
+
+
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+
+            Intent intent = new Intent(EmailPasswordActivity.this, ListTasks.class);
+            startActivity(intent);
+
+        }
+
+
+    }// end onCreate
+
+
+    @Override
+    public void onClick(View v) {//S onClick
+
+        if (v.getId() == R.id.btn_Login) {
+            signing(et_email.getText().toString(), et_password.getText().toString());
+
+        } else if (v.getId() == R.id.btn_LinkToRegisterScreen) {
+            registration(et_email.getText().toString(), et_password.getText().toString());
+
+        }
+
+
+    }// end onClick
+
+
+    public void signing(String email, String password) {// S signing
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                // matai she hametoda tase imut az itbatsea >> onComplete
+                if (task.isSuccessful()) {
+                    Toast.makeText(EmailPasswordActivity.this, "Authorization is successful", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(EmailPasswordActivity.this, ListTasks.class);
+                    startActivity(intent);
+
+
+                } else {
+                    Toast.makeText(EmailPasswordActivity.this, "Authorization is Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }//end signing
+
+
+    private void registration(String email, String password) {//S registration
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(EmailPasswordActivity.this, "Registration is successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(EmailPasswordActivity.this, "Registration is Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }//end registration
+
+
+}// END EmailPasswordActivity
---
+++ app/src/main/java/com/loftblog/fb_test_3/ListTasks.java
@@ -0,0 +1,110 @@
+package com.loftblog.fb_test_3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
+public class ListTasks extends AppCompatActivity {// Start ListTasks
+
+
+    public FirebaseAuth mAuth;
+    private DatabaseReference myRef;
+
+    FirebaseUser user = mAuth.getInstance().getCurrentUser();
+
+    FirebaseListAdapter mAdapter;
+
+    private Button btn_new_task; // ET_new_task
+    private EditText et_new_task;// Btn_new_task
+
+   // ListView listViewUserTasks; // ListUserTasks
+
+    private static class TaskViewHolder extends RecyclerView.ViewHolder{
+
+        TextView mTitleTask;
+        Button mDel;
+
+        public TaskViewHolder(View itemView) {
+            super(itemView);
+
+            mTitleTask = (TextView)itemView.findViewById(R.id.tv_title_task);
+            mDel = (Button)itemView.findViewById(R.id.btn_del); ////////// btn_add ???????
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {// S onCreate
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_tasks);
+
+     //   listViewUserTasks = (ListView) findViewById(R.id.discr_for_task);
+
+        myRef = FirebaseDatabase.getInstance().getReference(); // initsialeziruem nashu silku na bazu danih
+
+        // palu4im silku na defoltnuiu bazu danih
+      /*  mAdapter = new FirebaseListAdapter<String>(this, String.class, android.R.layout.simple_list_item_1, myRef.child(user.getUid()).child("Tasks")) {// peredav v ka4estve parametrav activiti class skatorim adapter budet rabotat
+            @Override
+            protected void populateView(View v, String s, int position) {// polu4aet na vhod object view nashu straku i pozitsiu dlya izmeneniya faila maketa >>>>>>>>>> metod budit vizivatsa kazdiy raz kakda danie budut izmeneny
+                TextView text = (TextView) v.findViewById(android.R.id.text1);
+                text.setText(s);
+            }
+        };
+        // ustanovim nash adapter
+        listViewUserTasks.setAdapter(mAdapter);
+
+        // pereidyom k dabavleneu danih >> naydyom nashi polya dlya voda i knopku dabavit
+        btn_new_task = (Button) findViewById(R.id.btn_add);
+
+        et_new_task = (EditText) findViewById(R.id.et_new_tasks);
+
+        btn_new_task.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.child(user.getUid()).child("Tasks").push().setValue(et_new_task.getText().toString());
+            }
+        });
+    */
+
+      // dabavleniya danih v firebase database
+        btn_new_task = (Button) findViewById(R.id.btn_add);
+        et_new_task = (EditText) findViewById(R.id.et_new_tasks);
+
+        btn_new_task.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.child(user.getUid()).
+                        child("Tasks").
+                        push().
+                        setValue(et_new_task.getText().toString());
+            }
+        });
+
+       RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_list_tasks) ;
+
+        // budim peredavat danie4
+        FirebaseListAdapter<String,TaskViewHolder> adapter;
+
+
+    }//end onCreate
+
+
+}//END  ListTasks
